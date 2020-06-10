e07920a344eee915cc9dd283f9d08cd633a87ec8
04vibs <04vibs@gmail.com>
2020-06-10 14:06:50 +0530

    Input property

--- src/app/todo-detail/todo-detail.component.ts
+++ src/app/todo-detail/todo-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-todo-detail',
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TodoDetailComponent implements OnInit {
 
   constructor() { }
+  @Input() selectedTodo: any;
 
   ngOnInit(): void {
   }
